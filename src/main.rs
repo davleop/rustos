673c82a1975aef6bc34f9565df302598894b6e72
davleop <davleop1999@gmail.com>
2022-01-27 22:02:27 -0600

    add test cases capabilities

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -16,5 +21,24 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial_assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
