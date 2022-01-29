5720221c20d9d96cb1fa70b06212adc34c9532ef
david <davleop1999@gmail.com>
2022-01-29 17:50:04 -0600

    start of allocation of frames

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,20 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use rustos::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{
+        structures::paging::{Translate, Page},
+        VirtAddr
+    };
 
     println!("Hello World{}", "!");
     rustos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     let addresses = [
         // the identity-mapped vga buffer page
@@ -35,6 +42,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         // virtual address mapped to physical address 0
         boot_info.physical_memory_offset,
     ];
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
--- src/memory.rs
+++ src/memory.rs
@@ -1,15 +1,64 @@
 // memory.rs
 
 use x86_64::{
-    structures::paging::{PageTable, OffsetPageTable},
+    structures::paging::{
+        Page, PhysFrame, PageTable, OffsetPageTable,
+        Mapper, Size4KiB, FrameAllocator
+    },
     VirtAddr,
     PhysAddr,
 };
+
+use bootloader::bootinfo::MemoryMap;
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    /// Create a FrameAllocator from the passed memory map.
+    ///
+    /// This function is unsafe because the caller must guarantee that the passed
+    /// memory map is valid. The main requirement is that all frames that are marked
+    /// as `USABLE` in it are really unused.
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
 
 pub unsafe fn init(physical_memory_offset: VirtAddr)
     -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
 }
 
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
